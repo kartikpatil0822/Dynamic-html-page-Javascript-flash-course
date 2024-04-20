9ee240b1646f224cdff82863d5b047988ef0fc71
kartikpatil2200 <kartikpatil2200@gmail.com>
2024-04-20 18:22:56 +0530

    Modified menu functionality for work

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function menuHandler() {
     
     
     document.querySelector("#close-nav-menu").addEventListener("click", function(){
-        document.querySelector("header nav .wrapper").classList.add("nav-open");
+        document.querySelector("header nav .wrapper").classList.remove("nav-open");
         // document.querySelector("header nav .wrapper").classList.toggle("nav-open"); -> another way to do
     });
 }
